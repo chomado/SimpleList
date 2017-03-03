eae60c46603fc03616fef90a067e1aa7f789ec67
chomado <chomado@gmail.com>
2017-03-03 15:14:49 +0900

    Speaker のプロパティ

--- SimpleList/Speaker.cs
+++ SimpleList/Speaker.cs
@@ -6,5 +6,13 @@ namespace SimpleList
         public Speaker()
         {
         }
+
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Website { get; set; }
+        public string Title { get; set; }
+        public string Avatar { get; set; }
     }
 }
