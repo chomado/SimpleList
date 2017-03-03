924ae671e78d170cb20a322ad222c2a992ad76a0
chomado <chomado@gmail.com>
2017-03-03 15:31:10 +0900

    "データを読み込む"ボタンが押された時の処理（イベント）

--- SimpleList/SimpleListPage.xaml.cs
+++ SimpleList/SimpleListPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Xamarin.Forms;
 
 namespace SimpleList
 {
@@ -7,6 +11,21 @@ namespace SimpleList
         public SimpleListPage()
         {
             InitializeComponent();
+        }
+
+        // "データを読み込む"ボタンが押された時の処理（イベント）
+        private async void OnClick(object sender, EventArgs e)
+        { 
+            using (var client = new HttpClient())
+            {
+                //サーバーから json を取得します
+                var json = await client.GetStringAsync("http://demo4404797.mockable.io/speakers");
+
+                //json をデシリアライズします
+                var items = JsonConvert.DeserializeObject<List<Speaker>>(json);
+
+                this.speakerListView.ItemsSource = items;
+            }
         }
     }
 }
